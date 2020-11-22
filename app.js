d4e600c1cba48f6722a0b446d8eb4145a259ec70
Esto Triramdani N <estotriramdani@gmail.com>
2020-11-22 17:43:47 +0700

    about update, initiate exp

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ $('.menu').click(function () {
     $('.content').load('home.html');
   } else if (menu == "about") {
     $('.content').load('about.html');
-  } else if (menu == "tutorial") {
-    $('.content').load('tutorial.html');
+  } else if (menu == "experiences") {
+    $('.content').load('experiences.html');
   } else if (menu == "sosmed") {
     $('.content').load('sosmed.html');
+  } else if (menu == "start") {
+    $('.content').load('about.html');
   }
 });
 
 // halaman yang di load default pertama kali
-$('.content').load('about.html');
+$('.content').load('experiences.html');
