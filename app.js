e65b0fac7b7d1c4c9c339a7fd91e48f47b7942f8
Esto Triramdani N <estotriramdani@gmail.com>
2020-12-10 18:46:43 +0700

    init

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-$('.menu').click(function () {
-  var menu = $(this).attr('id');
+$(".menu").click(function () {
+  var menu = $(this).attr("id");
   if (menu == "home") {
-    $('.content').load('home.html');
+    $(".content").load("home.html");
   } else if (menu == "about") {
-    $('.content').load('about.html');
+    $(".content").load("about.html");
   } else if (menu == "experiences") {
-    $('.content').load('experiences.html');
+    $(".content").load("experiences.html");
   } else if (menu == "completions") {
-    $('.content').load('completions.html');
+    $(".content").load("completions.html");
   } else if (menu == "start") {
-    $('.content').load('about.html');
+    $(".content").load("about.html");
   } else if (menu == "portfolios") {
-    $('.content').load('portfolios.html');
+    $(".content").load("portfolios.html");
   }
 });
 
-$('.content').load('home.html');
+$(".content").load("home.html");
