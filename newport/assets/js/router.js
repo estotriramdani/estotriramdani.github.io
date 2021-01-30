ca6bd88ae4464e65bb2769adf80462b9f6716193
Esto Triramdani N <estotriramdani@gmail.com>
2021-01-31 00:09:25 +0700

    update newport

--- newport/assets/js/router.js
+++ newport/assets/js/router.js
@@ -1,14 +1,11 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
   $routeProvider
-    .when("/first", {
-      templateUrl: "first.html",
-    })
-    .when("/pages/second", {
-      templateUrl: "pages/second.html",
-    })
     .when("/", {
-      templateUrl: "pages/home.html",
+      templateUrl: "pages/profile.html",
+    })
+    .when("/profile", {
+      templateUrl: "pages/profile.html",
     })
     .when("/blue", {
       templateUrl: "blue.htm",
