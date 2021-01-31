6168e0bee49018617235ac0dc3360d27fcf068ef
Esto Triramdani N <estotriramdani@gmail.com>
2021-01-31 20:14:47 +0700

    new portfolio website

--- script.js
+++
@@ -1,7 +0,0 @@
-$(".name").click(function () {
-  $(".name-layer").css("transform", "rotate(6deg)");
-});
-
-$(".name").mouseleave(function () {
-  $(".name-layer").css("transform", "rotate(0deg)");
-});
